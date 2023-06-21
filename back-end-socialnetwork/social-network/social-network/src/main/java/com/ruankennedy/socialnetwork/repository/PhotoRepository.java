e6aaf43c7f7f9ab40010cd2175f226a536b9eb14
RuanK07 <ruank07@gmail.com>
2023-06-21 11:19:09 -0300

    Back-PhotoRepository

---
+++ back-end-socialnetwork/social-network/social-network/src/main/java/com/ruankennedy/socialnetwork/repository/PhotoRepository.java
@@ -0,0 +1,9 @@
+package com.ruankennedy.socialnetwork.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ruankennedy.socialnetwork.model.Photo;
+
+public interface PhotoRepository extends JpaRepository<Photo, String>{
+
+}
