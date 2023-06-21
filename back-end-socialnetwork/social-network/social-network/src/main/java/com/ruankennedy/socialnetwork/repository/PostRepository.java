a766063e15698f388890c7deb093ce391e982327
RuanK07 <ruank07@gmail.com>
2023-06-21 11:18:54 -0300

    Back-PostRepository

---
+++ back-end-socialnetwork/social-network/social-network/src/main/java/com/ruankennedy/socialnetwork/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.ruankennedy.socialnetwork.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ruankennedy.socialnetwork.model.Post;
+
+public interface PostRepository extends JpaRepository<Post, String>{
+	
+	Optional<Post> findBySubtilte(String subtilte);
+	
+}
