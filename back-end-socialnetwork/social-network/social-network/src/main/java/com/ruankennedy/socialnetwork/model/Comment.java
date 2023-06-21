a6a57602cc3a47877afbe874f2d81ab8739d1895
RuanK07 <ruank07@gmail.com>
2023-06-21 11:16:46 -0300

    Back-CommentModel

---
+++ back-end-socialnetwork/social-network/social-network/src/main/java/com/ruankennedy/socialnetwork/model/Comment.java
@@ -0,0 +1,43 @@
+package com.ruankennedy.socialnetwork.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_comments")
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Comment {
+
+	@Id
+    @EqualsAndHashCode.Include
+    @Column(name = "ID", nullable = false, unique = true)
+    private final String id = UUID.randomUUID().toString();
+
+    @Column(name = "COMMENT", nullable = false, unique = true)
+    private String comment;
+    
+    @Column(name = "COMMENTED_MOMENT", nullable = false)
+    @CreationTimestamp
+    @Setter(AccessLevel.NONE)
+    private LocalDateTime commentedMoment;
+    
+    @ManyToOne
+    private Post post;
+    
+}
