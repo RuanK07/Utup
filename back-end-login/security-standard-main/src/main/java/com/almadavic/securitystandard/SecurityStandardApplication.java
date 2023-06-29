ad51a7f7def31acb0459fd9699ccfc00d1a90687
RuanK07 <ruank07@gmail.com>
2023-06-29 16:29:47 -0300

    security-standard/Remove unused imports

--- back-end-login/security-standard-main/src/main/java/com/almadavic/securitystandard/SecurityStandardApplication.java
+++ back-end-login/security-standard-main/src/main/java/com/almadavic/securitystandard/SecurityStandardApplication.java
@@ -4,10 +4,7 @@ package com.almadavic.securitystandard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
--- back-end-login/security-standard-main/src/main/java/com/almadavic/securitystandard/service/serviceAction/AuthenticationServiceImpl.java
+++ back-end-login/security-standard-main/src/main/java/com/almadavic/securitystandard/service/serviceAction/AuthenticationServiceImpl.java
@@ -7,8 +7,6 @@ import com.almadavic.securitystandard.service.customException.DatabaseException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 
