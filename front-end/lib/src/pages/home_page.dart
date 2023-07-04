031f2e64f6b97117723cfbafb82ab1e125c4f51d
RuanK07 <ruank07@gmail.com>
2023-07-04 17:21:30 -0300

    front-end/Bottom Navegation

--- front-end/lib/src/pages/home_page.dart
+++ front-end/lib/src/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:seutup/src/routes/routes.dart';
+import 'package:seutup/src/pages/profile_page.dart';
+import 'package:seutup/src/pages/search_page.dart';
+
+import 'favorite_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,59 +13,53 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
+  late PageController pc;
 
-  final List<String> _pageRoutes = [
-    AppRoutes.home,
-    AppRoutes.search,
-    AppRoutes.post,
-    AppRoutes.favorite,
-    AppRoutes.profile,
-  ];
+  @override
+  void initState() {
+    super.initState();
+    pc = PageController(initialPage: _currentIndex);
+  }
 
-  void _onTabTapped(int index) {
+  setActualPage(page) {
     setState(() {
-      _currentIndex = index;
+      _currentIndex = page;
     });
-
-    Navigator.pushNamed(context, _pageRoutes[index]);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Conteúdo da página $_currentIndex'),
+      body: PageView(
+        controller: pc,
+        children: [
+          SearchPage(),
+          FavoritePage(),
+          ProfilePage(),
+        ],
+        onPageChanged: setActualPage,
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
-        onTap: _onTabTapped,
-        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favorite'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Profile',
+          )
+        ],
         selectedItemColor: Colors.black,
         unselectedItemColor: Colors.grey,
         showSelectedLabels: false,
         showUnselectedLabels: false,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_box),
-            label: 'Add',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.star),
-            label: 'Likes',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: 'Profile',
-          ),
-        ],
+        onTap: (page) {
+          pc.animateToPage(
+            page,
+            duration: Duration(milliseconds: 400),
+            curve: Curves.ease,
+          );
+        },
       ),
     );
   }
