cfa04a0e36610b4048a0d80d6283a6c298d7f1f5
RuanK07 <ruank07@gmail.com>
2023-08-24 18:49:04 -0300

    Add Unit tests to ProfileService

--- back-end-socialnetwork/social-network/social-network/src/test/java/com/ruankennedy/socialnetwork/service/profileService/ProfileServiceTest.java
+++ back-end-socialnetwork/social-network/social-network/src/test/java/com/ruankennedy/socialnetwork/service/profileService/ProfileServiceTest.java
@@ -1,102 +1,97 @@
 package com.ruankennedy.socialnetwork.service.profileService;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import com.ruankennedy.socialnetwork.dto.request.ChangeBiography;
 import com.ruankennedy.socialnetwork.dto.request.ChangeProfilePhoto;
-import com.ruankennedy.socialnetwork.dto.response.ProfileDTO;
 import com.ruankennedy.socialnetwork.model.Profile;
 import com.ruankennedy.socialnetwork.model.User;
 import com.ruankennedy.socialnetwork.repository.ProfileRepository;
 import com.ruankennedy.socialnetwork.repository.UserRepository;
+import com.ruankennedy.socialnetwork.service.customException.ResourceNotFoundException;
 import com.ruankennedy.socialnetwork.service.serviceAction.ProfileServiceImpl;
+
+import jakarta.transaction.Transactional;
+
 
 @SpringBootTest
 public class ProfileServiceTest {
-    @Mock
-    private UserRepository userRepository;
-    
-    @Mock
+
+	@Autowired
     private ProfileRepository profileRepository;
-    
-    @InjectMocks
+
+    @Autowired
     private ProfileServiceImpl profileService;
+    
+    @Autowired
+    private UserRepository userRepository;
 
-    @BeforeEach
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
+    @Test
+    @Transactional
+    public void testGetProfileById() {
+    	
+    	User user = userRepository.findByEmail("admin@hotmail.com") 
+                .orElseThrow(()-> new ResourceNotFoundException("The user wasn't found on database\""));
+    	
+    	String userId = user.getId();
+    	
+        Profile profile = profileRepository.findByUserId(userId);
+        
+        String profileId = profile.getId();
+        
+        Profile profile2 = profileService.getProfileById(profileId);
+        
+        Assertions.assertEquals(profileId, profile2.getId());
+    }
+    
+    @Test
+    @Transactional
+    public void testUpdateProfilePhoto() {
+
+    	User user = userRepository.findByEmail("admin@hotmail.com")
+                .orElseThrow(()-> new ResourceNotFoundException("The user wasn't found on database\""));
+
+        byte[] newProfilePhoto = loadProfilePhotoAsByteArray("static/images/setup2.png");
+
+        ChangeProfilePhoto request = new ChangeProfilePhoto(newProfilePhoto);
+
+        Profile updatedProfile = profileService.updateProfilePhoto(user, request);
+
+        Assertions.assertArrayEquals(newProfilePhoto, updatedProfile.getProfilePhoto());
     }
 
     @Test
-    public void changeProfilePhotoTest() throws IOException {
-        String userId = "user1";
-        String imagePath = "/social-network/src/test/java/com/ruankennedy/socialnetwork/image/test.png";
-        Path path = Path.of(imagePath);
-        byte[] photoData = Files.readAllBytes(path);
-        ChangeProfilePhoto changeProfilePhoto = new ChangeProfilePhoto(photoData);
-        
-        User user = new User();
-        Profile profile = new Profile();
-        user.setProfile(profile);
-        
-        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
-        
-        profileService.changeProfilePhoto(userId, changeProfilePhoto);
-        
-        verify(userRepository).findById(userId);
-        verify(profileRepository).save(profile);
-        
-        Assertions.assertEquals(changeProfilePhoto.getProfilePhoto(), profile.getProfilePhoto());
+    @Transactional
+    public void testUpdateBiography() {
+
+    	User user = userRepository.findByEmail("admin@hotmail.com")
+                .orElseThrow(()-> new ResourceNotFoundException("The user wasn't found on database\""));
+
+        String newBiography = "NovaBiografia";
+
+        ChangeBiography request = new ChangeBiography(newBiography);
+
+        Profile updatedProfile = profileService.updateBiography(user, request);
+
+        Assertions.assertEquals(newBiography, updatedProfile.getBiography());
+    } 
+    
+    private byte[] loadProfilePhotoAsByteArray(String filePath) {
+        try {
+            Resource resource = new ClassPathResource(filePath);
+            return Files.readAllBytes(resource.getFile().toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    @Test
-    public void changeBiographyTest() {
-        String userId = "user1";
-        ChangeBiography changeBiography = new ChangeBiography("New biography");
-        
-        User user = new User();
-        Profile profile = new Profile();
-        user.setProfile(profile);
-        
-        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
-        
-        profileService.changeBiography(userId, changeBiography);
-        
-        verify(userRepository).findById(userId);
-        verify(profileRepository).save(profile);
-        
-        Assertions.assertEquals(changeBiography.getBiography(), profile.getBiography());
-    }
-
-    @Test
-    public void getProfileByNicknameTest() {
-        String nickname = "user1";
-        
-        User user = new User();
-        Profile profile = new Profile();
-        user.setProfile(profile);
-        
-        when(userRepository.findByNickname(nickname)).thenReturn(Optional.of(user));
-        
-        ProfileDTO result = profileService.getProfileByNickname(nickname);
-        
-        verify(userRepository).findByNickname(nickname);
-        
-        Assertions.assertEquals(profile.getProfilePhoto(), result.getProfilePhoto());
-        Assertions.assertEquals(profile.getBiography(), result.getBiography());
-    }
 }
